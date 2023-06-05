f0ce04bd41706598642b259276dc7e860c4517e8
Jonuz Miftari <jonuz.miftari@wsa.com>
2023-06-05 19:20:25 +0200

    Fixed usings

--- Domain/Entities/CoverAudit.cs
+++ Domain/Entities/CoverAudit.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Domain.Common;
 
 namespace Domain.Entities;
-internal class CoverAudit
+public class CoverAudit : AuditorBaseEntity
 {
+    public string? CoverId { get; set; }
 }
